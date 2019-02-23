17bc7472bc9a3e8d988482c123c9f573c3060233
TaylorDaly <taylor.daly@ucdenver.edu>
2019-02-22 17:36:50 -0700

    Pages are sorted by parent alphabetically now

--- node-api/Controllers/UserController.js
+++ node-api/Controllers/UserController.js
@@ -12,19 +12,19 @@ const Image = require('../models/Image');
 // accessing the register form.
 UserRouter.post('/verify', (req, res) => {
     let token = req.body.token;
-    if (!token) return res.status(403).json({auth: false, message: 'No token provided.'});
+    if (!token) return res.status(403).json({ auth: false, message: 'No token provided.' });
 
     jwt.verify(token, dbConfig.registerSecret, function (err, decoded) {
         if (err) {
-            return res.status(403).json({auth: false, message: 'Failed to authenticate token.'})
+            return res.status(403).json({ auth: false, message: 'Failed to authenticate token.' })
         } else {
-            Person.findOne({email: decoded.email}, 'email', (err, person) => {
+            Person.findOne({ email: decoded.email }, 'email', (err, person) => {
                 if (err) {
                     return res.status(500).json({
                         auth: false, message: `Something broke when attempting to find user. Error: ${err}`
                     })
                 } else if (person) {
-                    return res.status(409).json({auth: false, message: `Email already exists.`})
+                    return res.status(409).json({ auth: false, message: `Email already exists.` })
                 } else {
                     return res.json({
                         email: decoded.email,
@@ -40,11 +40,11 @@ UserRouter.post('/verify', (req, res) => {
 // Sign up using token from register email.
 UserRouter.post('/signup/:token', (req, res) => {
     let token = req.params.token;
-    if (!token) return res.status(400).json({auth: false, message: 'No token provided.'});
+    if (!token) return res.status(400).json({ auth: false, message: 'No token provided.' });
 
     jwt.verify(token, dbConfig.registerSecret, function (err, decoded) {
         if (err) {
-            return res.status(401).json({auth: false, message: 'Failed to authenticate token.'})
+            return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' })
         } else {
             let newPerson = new Person({
                 name: req.body.name,
@@ -87,7 +87,7 @@ UserRouter.post('/signup/:token', (req, res) => {
                                             success: false, message: `Failed to add new user. Error: ${err}`
                                         })
                                     } else {
-                                        res.json({success: true, message: "Successfully added person."});
+                                        res.json({ success: true, message: 'Successfully added person.' });
                                     }
                                 })
                             }
@@ -112,37 +112,36 @@ UserRouter.post('/signup/:token', (req, res) => {
 UserRouter.post('/login', (req, res) => {
     let password = req.body.password;
     let email = req.body.email;
-    Person.findOne({email: email}, 'email password _id', (err, person) => {
+    Person.findOne({ email: email }, 'email password _id', (err, person) => {
         if (err) {
             return res.status(500).json({
                 success: false, message: `Something broke when attempting to find user. Error: ${err}`
             })
         } else if (person) {
             bcrypt.compare(password, person.password, (err, result) => {
-                if (err)
+                if (err) {
                     return res.status(500).json({
                         success: false,
                         message: `Something broke when attempting to login. Error: ${err}`
                     });
-                else if (result) {
+                } else if (result) {
                     const jwtToken = jwt.sign({
-                            email: person.email,
-                            _id: person._id,
-                            sys_role: person.sys_role
-                        },
-                        dbConfig.secret,
-                        {
-                            expiresIn: '24h'
-                        });
-                    
+                        email: person.email,
+                        _id: person._id,
+                        sys_role: person.sys_role
+                    },
+                    dbConfig.secret,
+                    {
+                        expiresIn: '24h'
+                    });
+
                     return res.status(200).json({
                         success: true,
-                        message: "Login successful.",
+                        message: 'Login successful.',
                         role: person.sys_role,
                         token: jwtToken
                     });
-                } else
-                    return res.status(401).json({success: false, message: "Password incorrect."})
+                } else { return res.status(401).json({ success: false, message: 'Password incorrect.' }) }
             });
         } else {
             return res.status(400).json({
@@ -156,7 +155,7 @@ UserRouter.post('/login', (req, res) => {
 // Beginning of registration process, user must enter email and click link in email.
 UserRouter.post('/signup', (req, res) => {
     let email = req.body.email;
-    Person.findOne({email: email}, 'email', (err, person) => {
+    Person.findOne({ email: email }, 'email', (err, person) => {
         if (err) {
             res.status(500).json({
                 success: false,
@@ -170,7 +169,6 @@ UserRouter.post('/signup', (req, res) => {
         } else {
             // Verify email
             if (regex.email.exec(email)) {
-
                 let nodemailer = require('nodemailer');
 
                 let transporter = nodemailer.createTransport({
@@ -181,7 +179,7 @@ UserRouter.post('/signup', (req, res) => {
                     }
                 });
 
-                let token = jwt.sign({email: req.body.email}, dbConfig.registerSecret, {
+                let token = jwt.sign({ email: req.body.email }, dbConfig.registerSecret, {
                     expiresIn: 7200 // expires in 2 hours
                 });
 
@@ -199,7 +197,7 @@ UserRouter.post('/signup', (req, res) => {
                         `  </v:roundrect>\n<![endif]--><a href="${signupUrl}"\n` +
                         'style="background-color:#fafafb;border:1px solid #b1b1b9;border-radius:18px;color:#000000;display:' +
                         'inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:40px;text-align:center;' +
-                        'text-decoration:none;width:250px;-webkit-text-size-adjust:none;mso-hide:all;">Finish Sign Up</a></div>',
+                        'text-decoration:none;width:250px;-webkit-text-size-adjust:none;mso-hide:all;">Finish Sign Up</a></div>'
                 };
 
                 transporter.sendMail(mailOptions, function (error, info) {
